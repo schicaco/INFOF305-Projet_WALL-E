fd3bfdd06af820238533c084d89ad13e651db3f8
Phi Pham <phamdangphilong@gmail.com>
2022-12-03 15:20:27 +0100

    fig2 dessin trajectoires pour 5CI

--- simulations/sim1.m
+++ simulations/sim1.m
@@ -5,22 +5,26 @@ A = [2 3; 0 0];
 x1_values = A(1,1)*x1+A(1,2)*x2;
 x2_values = A(2,1)*x1+A(2,2)*x2;
 
+# Normalisation des valeurs pour la représentation graphique
 norms=sqrt(x1_values.^2+x2_values.^2);
 x1_normalized = x1_values ./ norms;
 x2_normalized = x2_values ./norms;
 
-# isoclines
+# Isoclines
 line_range = -1.5:.1:1.5;
 isocline_1 = -(A(1,1)/A(1,2)) * line_range;
+# La 2ième isocline n'existe pas (division par zéro)
 
 # Droites invariantes (vecteurs propres) 
 [V,L] = eig(A);
 eigenvector_1 = (V(2,1)/V(1,1)) * line_range;
 eigenvector_2 = (V(2,2)/V(1,2)) * line_range;
 
+# Figure 1: portrait de phase, isoclines et vecteurs propres
 figure(1);
 hold on;
 vector_scale = .5;
+
 quiver(x1,x2,x1_normalized,x2_normalized, vector_scale);
 
 plot(line_range,isocline_1,"linewidth", 3, "linestyle", ":");
@@ -28,4 +32,27 @@ plot(line_range,isocline_1,"linewidth", 3, "linestyle", ":");
 plot(line_range,eigenvector_1,"linewidth",3);
 plot(line_range,eigenvector_2,"linewidth",3);
 quiver([0;0],[0;0],V(1,:),V(2,:),"linewidth",3,"color","k");
+
+legend("portrait de phase","isocline","v_1","v_2","location","south");
+hold off;
+
+# Figure 2: trajectoires e(t), w(t) avec 5 conditions initiales
+figure(2);
+hold on;
+x1dot = @(t, x) A(1,1)*x(1)+A(1,2)*x(2);
+x2dot = @(t, x) A(2,1)*x(1)+A(2,2)*x(2);
+xdot = @(t,x) [x1dot(t, x);x2dot(t, x)];
+[t1,x1] = ode23(xdot, [0, 10], [2, 0]);
+[t2,x2] = ode23(xdot, [0, 10], [0, 3]);
+[t3,x3] = ode23(xdot, [0, 10], [-2, 3]);
+[t4,x4] = ode23(xdot, [0, 10], [2, -3]);
+[t5,x5] = ode23(xdot, [0, 10], [2, 2]);
+
+plot(t1,x1);
+plot(t2,x2);
+plot(t3,x3);
+plot(t4,x4);
+plot(t5,x5);
+# legend("CI: [2,0]","CI: [0,3]","CI: [-2,3]","location","northwest");
+# une legende pour 2 droites ? cmt faire ?
 hold off;
--- simulations/sim3.m
+++ simulations/sim3.m
@@ -12,8 +12,8 @@ norms=sqrt(x1_values.^2+x2_values.^2);
 x1_normalized = x1_values ./ norms;
 x2_normalized = x2_values ./norms;
 
+# Isoclines
 line_range = -1.5:.1:1.5;
-# isoclines
 isocline_1 = -(A(1,1)/A(1,2)) * line_range;
 isocline_2 = -(A(2,1)/A(2,2)) * line_range;
 
@@ -22,9 +22,11 @@ isocline_2 = -(A(2,1)/A(2,2)) * line_range;
 eigenvector_1 = (V(2,1)/V(1,1)) * line_range;
 eigenvector_2 = (V(2,2)/V(1,2)) * line_range;
 
+# Figure 1: portrait de phase, isoclines et vecteurs propres
 figure(1);
 hold on;
 vector_scale = .5;
+
 quiver(x1,x2,x1_normalized,x2_normalized, vector_scale);
 
 plot(line_range,isocline_1,"linewidth", 2, "linestyle", ":");
@@ -33,4 +35,26 @@ plot(line_range,isocline_2,"linewidth", 2, "linestyle", ":");
 plot(line_range,eigenvector_1,"linewidth",2);
 plot(line_range,eigenvector_2,"linewidth",2);
 quiver([0;0],[0;0],V(1,:),V(2,:),"linewidth",5,"color","k");
+
+legend("portrait de phase","isocline 1","isocline 2", "v_1","v_2","location","south");
+hold off;
+
+# Figure 2: trajectoires e(t), w(t) avec 5 conditions initiales
+figure(2);
+hold on;
+x1dot = @(t, x) A(1,1)*x(1)+A(1,2)*x(2);
+x2dot = @(t, x) A(2,1)*x(1)+A(2,2)*x(2);
+xdot = @(t,x) [x1dot(t, x);x2dot(t, x)];
+[t1,x1] = ode23(xdot, [0, 10], [2, 0]);
+[t2,x2] = ode23(xdot, [0, 10], [0, 3]);
+[t3,x3] = ode23(xdot, [0, 10], [-2, 3]);
+[t4,x4] = ode23(xdot, [0, 10], [2, -3]);
+[t5,x5] = ode23(xdot, [0, 10], [2, 2]);
+
+plot(t1,x1);
+plot(t2,x2);
+plot(t3,x3);
+plot(t4,x4);
+plot(t5,x5);
+
 hold off;
